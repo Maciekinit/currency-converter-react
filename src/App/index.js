f7eae51dd98c3857a3f221a68649b2df7ee9a876
Maciej Blaszczyk <programinghub64@gmail.com>
2025-10-01 22:31:42 +0200

    Changing the react component

--- src/App/index.js
+++ src/App/index.js
@@ -1,34 +1,28 @@
 import { useState } from 'react';
 import { Form } from './Form';
-import { Clock } from './Clock';
-import { currencies } from './currencies';
+import { StyledClock } from "./Clock";
+import { currencies } from "./currencies";
 import { Wrapper } from "./styled";
 
 function App() {
+  const [result, setResult] = useState();
 
-    const [result, setResult] = useState();
+  const calculateResult = (currency, amount) => {
+    const rate = currencies.find(({ short }) => short === currency).rate;
 
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-            .find(({ short }) => short === currency)
-            .rate;
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        });
-    }
-
-    return (
-        <Wrapper>
-            <Clock />
-            <Form
-                result={result}
-                calculateResult={calculateResult}
-            />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <StyledClock />
+      <Form result={result} calculateResult={calculateResult} />
+    </Wrapper>
+  );
 }
 
 export default App;
