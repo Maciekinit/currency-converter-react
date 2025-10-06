7a072e1e8b82eb91b989d7284608aa756f829b7e
Maciej Blaszczyk <programinghub64@gmail.com>
2025-10-06 17:01:45 +0200

    moving this function to Form

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { currencies } from "../currencies";
 import { Result } from "./Result";
 import {
     Button,
@@ -7,10 +6,27 @@ import {
     Header,
     Info,
     LabelText,
+    Loading,
+    Failure,
 } from "./styled";
 
-export const Form = ({ calculateResult, result }) => {
-    const [currency, setCurrency] = useState(currencies[0].short);
+import { useRatesData } from "./useRatesData";
+
+export const Form = () => {
+    const [result, setResult] = useState();
+    const retesData = useRatesData();
+
+    const calculateResult = (currency, amount) => {
+        const rate = retesData.rates[currency];
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount * rate,
+            currency,
+        });
+    }
+
+    const [currency, setCurrency] = useState("EUR");
     const [amount, setAmount] = useState("");
 
     const onSubmit = (event) => {
@@ -23,47 +39,63 @@ export const Form = ({ calculateResult, result }) => {
             <Header>
                 Przelicznik walut
             </Header>
-            <p>
-                <label>
-                    <LabelText> Kwota w zł*:</LabelText>
-                    <Field
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        placeholder="Wpisz kwotę w zł"
-                        type="number"
-                        required
-                        step="0.01"
-                    />
-                </label>
-            </p>
-            <p>
-                <label>
-                    <LabelText>Waluta:</LabelText>
-                    <Field
-                        as="select"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                    >
-                        {currencies.map((currency => (
-                            <option
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        )))}
-                    </Field>
-                </label>
-            </p>
-            <p>
-                <Button>Przelicz!</Button>
-            </p>
+            {retesData.state === "loading"
+                ? (
+                    <Loading>
+                        Ładowanie kursów walut...
+                    </Loading>
+                )
+                : (
+                    retesData.state === "error" ? (
+                        <Failure>
+                            Coś poszło nie tak. Spróbuj ponownie później.
+                        </Failure>
+                    ) : (
+                        <>
+                                <p>
+                                    < label >
+                                        <LabelText> Kwota w zł*:</LabelText>
+                                        <Field
+                                            value={amount}
+                                            onChange={({ target }) => setAmount(target.value)}
+                                            placeholder="Wpisz kwotę w zł"
+                                            type="number"
+                                            required
+                                            step="0.01"
+                                        />
+                                    </label>
+                                </p >
+                                <p>
+                                    <label>
+                                        <LabelText>Waluta:</LabelText>
+                                        <Field
+                                            as="select"
+                                            value={currency}
+                                            onChange={({ target }) => setCurrency(target.value)}
+                                        >
+                                            {Object.keys(retesData.rates).map(((currency) => (
+                                                <option
+                                                key={currency}
+                                                value={currency}
+                                            >
+                                                {currency}
+                                            </option>
+                                        )))}
+                                        </Field>
+                                    </label>
+                                </p>
+                                <p>
+                                    <Button>Przelicz!</Button>
+                                </p>
 
-            <Info>
-                Kursy pochodzą ze strony mbanku z dnia 2024-09-27.
-            </Info>
+                                <Info>
+                                    Kursy są pobierane z Europejskiego Banku Centralnego.
+                                </Info>
 
-            <Result result={result} />
-        </form >
+                                <Result result={result} />
+                        </>
+                    )
+                )}
+        </form>
     );
 };
--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,13 @@
-import { useState } from 'react';
 import { Form } from './Form';
 import { StyledClock } from "./Clock";
-import { currencies } from "./currencies";
 import { Wrapper } from "./styled";
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
 
   return (
     <Wrapper>
       <StyledClock />
-      <Form result={result} calculateResult={calculateResult} />
+      <Form />
     </Wrapper>
   );
 }
